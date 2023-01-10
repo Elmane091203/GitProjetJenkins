d69d99f89d4f37240b3c7aa0227acf2e9e8cf67c
Elmane091203 <djaanffarhoumadielmane@gmail.com>
2023-01-10 19:56:10 +0000

    projet sans test

--- springback/src/test/java/com/projet/springback/SpringbackApplicationTests.java
+++ springback/src/test/java/com/projet/springback/SpringbackApplicationTests.java
@@ -7,14 +7,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbackApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-    @Autowired
-    private GroupeServiceImpl groupeService;
-    @Test
-    void testRand(){
-        System.out.println(( ((groupeService.gener(4)!=null)?groupeService.gener(4).size():4) ==4)?"passer":"echec");
-    }
 }
